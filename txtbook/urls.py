ce8cce5dfa4cb17fbcec091dc4184dcc90d12928
Jared Hood <jwh6ry@virginia.edu>
2020-02-12 21:17:30 -0500

    Created basic index view

---
+++ txtbook/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'txtbook'
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- txtbook/views.py
+++ txtbook/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from django.template import loader
+
+
+def index(request):
+    return render(request, 'txtbook/index.html')
